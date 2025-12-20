17ebc3d2cb17307514921f4d4941a75e35e60771
Burak Özdemir <mburak.ozdemir03@gmail.com>
2025-12-20 14:59:52 +0300

    Add: favorites VC

--- Cleanzy/Screens/Favorites/FavoritesViewController.swift
+++ Cleanzy/Screens/Favorites/FavoritesViewController.swift
@@ -12,6 +12,20 @@ final class FavoritesViewController: UIViewController {
     
     // MARK: - Properties
     
+    private lazy var favoritesCollectionView: UICollectionView = {
+        let flowLayout: UICollectionViewFlowLayout = .init()
+        flowLayout.scrollDirection = .vertical
+        flowLayout.minimumLineSpacing = 16
+        flowLayout.minimumInteritemSpacing = 16
+        flowLayout.itemSize = CGSize(width: 420, height: 144)
+        
+        let collectionView: UICollectionView = .init(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = .white
+        return collectionView
+    }()
+    
     var presenter: FavoritesPresenterProtocol!
     
     // MARK: - Life Cycles
@@ -31,14 +45,22 @@ private extension FavoritesViewController {
         configureLayout()
         
         view.backgroundColor = .white
+        
+        navigationItem.title = "Favorilerim"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.black]
     }
     
     func addViews() {
-        
+        view.addSubviews([
+            favoritesCollectionView
+        ])
     }
     
     func configureLayout() {
-        
+        favoritesCollectionView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
     }
 }
 
@@ -46,4 +68,26 @@ private extension FavoritesViewController {
 
 extension FavoritesViewController: FavoritesViewProtocol {
     
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension FavoritesViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        .init()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        .init()
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension FavoritesViewController: UICollectionViewDelegate {
+    
+}
+
+#Preview {
+    FavoritesViewController()
 }
